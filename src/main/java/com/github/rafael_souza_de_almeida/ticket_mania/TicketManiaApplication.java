039457d1321c3d0e54762fe1c6d3cc09c51e78d2
rafael-almeida <rafaelalmeida37@ufrrj.br>
2026-06-14 21:12:30 -0300

    initial commit

---
+++ src/main/java/com/github/rafael_souza_de_almeida/ticket_mania/TicketManiaApplication.java
@@ -0,0 +1,13 @@
+package com.github.rafael_souza_de_almeida.ticket_mania;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketManiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketManiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/rafael_souza_de_almeida/ticket_mania/TicketManiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.rafael_souza_de_almeida.ticket_mania;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketManiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
