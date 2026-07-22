d7bf495c8afeff83b2f5de669a0bcbdf235a97c3
rafael-almeida <rafaelalmeida37@ufrrj.br>
2026-07-22 17:53:19 -0300

    fix: update order fulfillment process to include ticket status change

--- src/main/java/com/github/rafael_souza_de_almeida/ticket_mania/order/service/OrderPaymentService.java
+++ src/main/java/com/github/rafael_souza_de_almeida/ticket_mania/order/service/OrderPaymentService.java
@@ -2,9 +2,13 @@ package com.github.rafael_souza_de_almeida.ticket_mania.order.service;
 
 import com.github.rafael_souza_de_almeida.ticket_mania.core.rabbitMq.RabbitMQConfig;
 import com.github.rafael_souza_de_almeida.ticket_mania.order.domain.Order;
+import com.github.rafael_souza_de_almeida.ticket_mania.order.domain.Ticket;
 import com.github.rafael_souza_de_almeida.ticket_mania.order.domain.enums.OrderStatus;
+import com.github.rafael_souza_de_almeida.ticket_mania.order.domain.enums.TicketStatus;
 import com.github.rafael_souza_de_almeida.ticket_mania.order.exception.OrderNotFoundException;
 import com.github.rafael_souza_de_almeida.ticket_mania.order.repository.OrderRepository;
+import com.github.rafael_souza_de_almeida.ticket_mania.order.repository.TicketRepository;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
@@ -17,6 +21,7 @@ import java.util.UUID;
 public class OrderPaymentService {
 
     private final OrderRepository orderRepository;
+    private final TicketRepository ticketRepository;
     private final RabbitTemplate rabbitTemplate;
 
     @Transactional
@@ -29,8 +34,14 @@ public class OrderPaymentService {
             throw new IllegalStateException("Order is already fulfilled and paid.");
         }
 
+        Ticket ticket = ticketRepository.findById(order.getTicket().getId())
+                .orElseThrow(() -> new IllegalStateException("Associated ticket not found"));
+
         order.setStatus(OrderStatus.COMPLETED);
         orderRepository.save(order);
+
+        ticket.setStatus(TicketStatus.SOLD);
+        ticketRepository.save(ticket);
 
         rabbitTemplate.convertAndSend(
                 RabbitMQConfig.EXCHANGE_NAME,
